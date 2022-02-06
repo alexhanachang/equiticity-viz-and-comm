d371a6af331a04f0c421a1d41bda29d25c0c2f4c
Alex Chang <alexandrachang2022@u.northwestern.edu>
2022-02-06 15:22:01 -0600

    practicing shiny

--- app/app.R
+++ app/app.R
@@ -7,15 +7,25 @@ library(shiny)
 library(thematic)
 library(tidyverse)
 
-read_rds("census team data/Bike Routes/bikeRouteDataFort.RDS")
-read_rds("census team data/Bike Routes/roadsDataChi.RDS")
+bikeRouteDataFort <- read_rds("census team data/Bike Routes/bikeRouteDataFort.RDS")
+roadsDataChi <- read_rds("census team data/Bike Routes/roadsDataChi.RDS")
+
+map <- ggplot() +
+    geom_path(data = roadsDataChi,
+              aes(x = long, y = lat, group = group),
+              color = "red") +
+    geom_path(data = bikeRouteDataFort,
+              aes(x = long, y = lat, group = group),
+              color = "green")  +
+    theme_void() + 
+    labs(title = "Bike routes + roads in Chicago") + 
+    theme(title = element_text(color = "white"))
 
 ui <-
     navbarPage(
         theme = bslib::bs_theme(bootswatch = "darkly"), 
         "Equiticity",   
-        # tabPanel("map", mainPanel(plotOutput("plot"))),
-        tabPanel("demographics charts", "testing"),
+        tabPanel("map", plotOutput("plot")),
         tabPanel("panel 3", "three"),
         navbarMenu("subpanels", 
                    tabPanel("panel 4a", "four-a"),
@@ -26,28 +36,13 @@ ui <-
     ) %>% 
     
     fluidPage(
-        titlePanel("Divvy stations in Chicago"),
-        sidebarLayout(
-            sidebarPanel(
-                numericInput("m", "Number of samples:", 2, min = 1, max = 100)
-            )
-        )
     )
 
 
 server <- function(input, output, session) {
     thematic::thematic_shiny()
     
-    output$plot <- renderPlot({
-        ggplot() +
-            geom_path(data = roadsDataChi,
-                      aes(x = long, y = lat, group = group),
-                      color = "red") +
-            geom_path(data = bikeRouteDataFort,
-                      aes(x = long, y = lat, group = group),
-                      color = "green") +
-            theme_void()
-    })
+    output$plot <- renderPlot({map})
     
 }
 
