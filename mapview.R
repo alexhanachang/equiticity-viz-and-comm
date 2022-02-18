cdd4b83c186fb073c2c1a80ccdb16b7b8a10cfa7
Alex Chang <alexandrachang2022@u.northwestern.edu>
2022-02-18 14:50:41 -0600

    02.18.22

---
+++ mapview.R
@@ -0,0 +1,28 @@
+library(leaflet)
+library(leafpop)
+library(mapview)
+library(sf)
+library(skimr)
+library(spData)
+library(tidyverse)
+
+install::
+
+communities <- readRDS('data/communities.RDS')
+crimes <- read_csv('data/violence_enforcement/crimes_2021.csv')
+
+crimes_type <- crimes %>% 
+  mutate(community_id = as.numeric(community_area)) %>% 
+  group_by(community_id, primary_type) %>% 
+  summarise(num_crimes = n()) %>% 
+  filter(num_crimes == max(num_crimes))
+
+crimes_type_map <- communities %>% 
+  right_join(crimes_type, by = c('community_id' = 'community_id')) %>% 
+  select(community_id, community, primary_type, num_crimes, geometry)
+
+mapview(crimes_type_map, 
+        zcol = "primary_type")
+       # popup = leafpop::popupTable(crimes_type_map, zcol = c("community", "num_crimes")))
+
+
