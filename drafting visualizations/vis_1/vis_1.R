5b3607f8cc6542482cc66deac96e7a4a37fb178d
alex hana chang <alexandrachang2022@u.northwestern.edu>
2022-02-23 18:39:24 -0600

    02.24.22
    mapview

--- drafting visualizations/vis_1/vis_1.R
+++ drafting visualizations/vis_1/vis_1.R
@@ -1,97 +1,152 @@
-stations_rollout <- readRDS("data/stations_rollout.RDS")
+stations_rollout <- readRDS("data/stations_rollout.RDS") 
 communities <- readRDS("data/communities.RDS")
 
-library(mapview)
 library(gganimate)
+library(mapview)
 library(move)
 library(moveVis)
+library(tidyverse)
 library(viridis)
 
-
-pal <- magma(n = length(unique(stations_rollout$rollout_year)))
-
-mapview(communities, legend = FALSE) + 
-  mapview(stations_rollout, xcol = "lon", ycol = "lat", zcol = "rollout_year", col.regions = pal)
+mapviewOptions(fgb = FALSE)
 
 
-
-typeof(stations_rollout)
-as.data.frame(stations_rollout)
-
-stations_rollout %>% 
-  select(unique(stations_rollout$install_date))
-
-stations_rollout <- distinct(stations_rollout, install_date, .keep_all = T)
+library(RColorBrewer)
 
 
+mapview(communities) +
+  mapview(
+  (rollout_region %>%
+     filter(region == "Far Southwest Side")), 
+  zcol = "rollout_year", 
+  legend = TRUE, 
+  layer.name = "x", 
+  col.regions = brewer.pal(5, "RdPu")
+    )
 
-##################
+mapview(communities) +
+  mapview(
+    (rollout_region %>%
+       filter(rollout_year == "2013")), 
+    zcol = "region", 
+    legend = TRUE, 
+    layer.name = "x", 
+    col.regions = brewer.pal(5, "RdPu")
+  )
 
-test <- df2move(df = stations_rollout,
-                proj = "+init=epsg:4326 +proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0", 
-                x = "lon", y = "lat", time = "install_date")
+mapview(communities, zcol = "region",     
+        col.regions = brewer.pal(9,"Greys")) +
+  mapview(rollout_region, 
+          zcol = "rollout_year_binned", 
+    legend = TRUE, 
+    layer.name = "x", 
+    col.regions = brewer.pal(4,"RdPu"),
+    cex = 3
+  )
 
-y <- align_move(test, unit = "days") %>% 
-  head(10)
+stations_rollout$rollout_year %>% unique()
 
-frames <- frames_spatial(y)
-frames <- frames %>% 
-  add_gg(frames, gg = expr(geom_path(aes(x = x, y = y), 
-                                     colour = "blue", linetype = "dashed")))
+rollout_region <- rollout_region %>% 
+  mutate(rollout_year_binned = factor(rollout_year, 
+         levels = c(2013, 2015, 2016, 2017, 2018, 2019, 2020, 2021), 
+         labels = c("2013-2015", "2013-2015", "2016-2017", "2016-2017", 
+                    "2018-2019", "2018-2019", "2020-2021", "2020-2021")
+  ))
 
-
-animate_frames(frames, out_file = "t.gif")
-
-
-
-data("move_data", package = "moveVis") # move class object
-# if your tracks are present as data.frames, see df2move() for conversion
-
-# align move_data to a uniform time scale
-m <- align_move(move_data, res = 4, unit = "days")
-
-# create spatial frames with a OpenStreetMap watercolour map
-frames <- frames_spatial(m, path_colours = c("red", "green", "blue"),
-                         map_service = "osm", map_type = "watercolor", alpha = 0.5) %>%
-  add_labels(x = "Longitude", y = "Latitude") %>% # add some customizations, such as axis labels
-  add_northarrow() %>%
-  add_scalebar() %>%
-  add_timestamps(m, type = "label") %>%
-  add_progress()
+rollout_region <- st_join(stations_rollout, left = FALSE, communities)
 
 
-# animate frames
-animate_frames(frames, out_file = "moveVis.gif")
+stations_rollout %>% add_column(region = NA)
 
-library(magrittr)
-library(tibble)
-library(leaflet)
-library(leaflet.extras2)
-library(sf)
-library(lubridate)
-library(leaflet.extras2)
+isd_ca_co_pts <- st_join(isd_history, left = FALSE, ca_co["name"]) # join points
+st_join(stations_rollout, left = FALSE, communities["community"])
+st_join(communities, left = FALSE, stations_rollout)
 
-# how many test data points to create
-num_points <- 100
-df <- tibble::tibble(temp = (1:num_points),
-                     lat = seq(from = 45, to = 46, length.out = num_points) + .1*sin(temp),
-                     lon = seq(from = -75, to = -75.5, length.out = num_points) + .1*cos(temp),
-                     datetime = seq(from = lubridate::ymd_hms("2021-09-01 8:00:00"),
-                                    to = lubridate::ymd_hms("2021-09-01 9:00:00"),
-                                    length.out = num_points)) %>%
-  sf::st_as_sf(coords = c("lon", "lat"), crs = "WGS84", remove = FALSE)
 
-leaflet() %>%
-  addTiles() %>%
-  leaflet.extras2::addPlayback(data = df,
-                               time = "datetime",
-                               options = leaflet.extras2::playbackOptions(speed = 100))
+# plot
+plot(isd_ca_co_pts$geometry, pch=21, cex=0.7, col="purple", bg="gray80")
+plot(ca_co$geometry, border="gray20", col=NA, add=T)
 
-leaflet() %>%
-  addTiles() %>%
-  leaflet.extras2::addPlayback(data = station_rollouts,
-                               time = "install_date",
-                               options = leaflet.extras2::playbackOptions(speed = 100))
 
-test <- st_as_sf(station_rollouts, coords = c("lon", "lat"), 
-                 crs = 4326, agr = "constant")
+plot(stations_rollout$geometry)
+
+### creating a function to filter by year
+stations_rollout <- stations_rollout %>% 
+  mutate(rollout_year = rollout_year %>% as.numeric())
+
+a <- stations_rollout %>% 
+  filter(stations_rollout$rollout_year[] == 2021)
+
+mapview(communities) + 
+  mapview(
+    a, 
+    xcol = "lon", 
+    ycol = "lat", 
+    grid = FALSE
+  )
+
+func <- function(x){
+  b <- stations_rollout %>% filter(stations_rollout$rollout_year[] == x)
+  print(
+    mapview(communities) + 
+      mapview(
+        b, 
+        xcol = "lon", 
+        ycol = "lat", 
+        grid = FALSE, 
+        zcol = "rollout_year"
+      )
+  )
+}
+
+func(2021)
+
+
+### creating a function to filter by region
+a <- stations_rollout %>% 
+  filter(stations_rollout$rollout_year[] == 2021)
+
+mapview(communities) + 
+  mapview(
+    a, 
+    xcol = "lon", 
+    ycol = "lat", 
+    grid = FALSE
+  )
+
+func <- function(x){
+  b <- stations_rollout %>% filter(stations_rollout$rollout_year[] == x)
+  print(
+    mapview(communities) + 
+      mapview(
+        b, 
+        xcol = "lon", 
+        ycol = "lat", 
+        grid = FALSE, 
+        zcol = "rollout_year"
+      )
+  )
+}
+
+func(2021)
+
+
+
+# Convert data frame to sf object
+stations_rollout <- st_as_sf(
+  x = stations_rollout, 
+  coords = c("lon", "lat"))
+
+st_crs(stations_rollout)
+
+st_crs(communities)
+
+communities <- st_set_crs(communities, 4326)
+stations_rollout <- st_set_crs(stations_rollout, 4326)
+
+
+
+
+
+
+
---
+++ mapview app.R
@@ -0,0 +1,73 @@
+library(shiny)
+library(mapview)
+library(tidyverse)
+library(leaflet)
+
+
+stations_rollout <- readRDS("data/stations_rollout.RDS") %>% 
+  mutate(rollout_year = rollout_year %>% as.numeric())
+communities <- readRDS("data/communities.RDS")
+
+
+mapviewOptions(fgb = FALSE)
+
+
+ui <- fluidPage(
+  # application title
+  titlePanel("Rollout map"),
+  
+  # sidebar with a slider input for number of bins 
+  sidebarLayout(
+    
+    # position sidebar
+   mainPanel(),
+     position = "right", 
+      # radio buttons for fill variable
+    #   radioButtons(
+    #     inputId = "year", 
+    #     label = "Select year", 
+    #     choices = list(
+    #       2013, 2014, 2015
+    #     ), 
+    #     selected = 2015
+    #   )
+    # ), 
+    
+    # show plot
+    mainPanel(
+      leafletOutput("distPlot")
+    )
+  ))
+
+
+server <- function(input, output) {
+  
+  
+  output$distPlot <- renderLeaflet({
+    
+    
+    # fill_hist <- bindEvent(
+    #   input$fill_var, 
+    #   "2013" = (stations_rollout %>% filter(stations_rollout$rollout_year[] == 2013)), 
+    #   "2014" = (stations_rollout %>% filter(stations_rollout$rollout_year[] == 2014)), 
+    #   "2015" = (stations_rollout %>% filter(stations_rollout$rollout_year[] == 2015))
+    # )
+    
+    # # building histogram
+    # if(is.null(fill_hist)){
+    #   
+    # }
+
+
+    (mapview(communities) + 
+      mapview(
+            stations_rollout %>% filter(stations_rollout$rollout_year[] == 2013), 
+            xcol = "lon", 
+            ycol = "lat", 
+            grid = FALSE
+          ))@map
+  })
+
+}
+
+shinyApp(ui, server)
